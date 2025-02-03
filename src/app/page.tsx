379d919b78359e54aec853759ea81f9c3028fb15
Fajar <fajaruddin.soleh@gmail.com>
2025-02-03 18:35:06 +0700

    fffff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
-    <Input type="email" placeholder="Email" />
+    <div>
+      <h1>jejfefiojeiojfiojewioj</h1>
+      <Input type="email" placeholder="Email" />
+      <h1>gwduiwhuhdwiuhdwuh</h1>
+    </div>
+
   );
 }
