18558820d3a85595732d24c6f01c5c6373f2fe94
Fajar <fajaruddin.soleh@gmail.com>
2025-02-07 10:56:09 +0700

    bikin halamman admin

---
+++ src/app/client/form/[id]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Child from '../../../page';
+
+const page = () => {
+  return (
+    <div>
+      <Child>
+        <h1>Hello World!</h1>
+        <p>This is a client component.</p>
+      </Child>
+    </div>
+  )
+}
+
+export default page
